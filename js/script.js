d169c9eced62e9738450a31f4c9f3c5968285788
omartins-zs <gabrielmatheus209@gmail.com>
2023-04-25 16:14:23 -0300

    Add Validação de Formulario com JS (ERRO)

--- js/script.js
+++ js/script.js
@@ -3,8 +3,19 @@ for (let input of inputs) {
     input.addEventListener("blur", function () {
         if (input.value.trim() != "") {
             input.classList.add('has-val')
-        }else{
+        } else {
             input.classList.remove('has-val')
         }
     })
 }
+
+let form = document.getElementById('login-form');
+form.addEventListener("submit", function (event) {
+    let inputs = document.getElementsByClassName('input-form');
+    for (let input of inputs) {
+        if (input.value.trim() == "") {
+            input.parentElement.classList.add("wrap-input-invalid");
+        }
+    }
+    event.preventDefault();
+})
